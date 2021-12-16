f70728c3b5644d8e8728558c1ee69ba37e345025
romaklimenko <romaklimenko@gmail.com>
2021-12-16 19:26:29 +0100

    /api/blob/[id]/[name] - returns file content

---
+++ pages/api/blob/[id]/[name].ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getBlobsCollection } from "../../../../lib/mongo";
+import { Storage } from '@google-cloud/storage';
+import contentType from "../../../../lib/contentType";
+import path from "path";
+
+const gcs_storage = new Storage();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const blobsCollection = await getBlobsCollection();
+
+  const id: string = req.query.id as string;
+  const name: string = req.query.name as string;
+
+  if (!id) {
+    res.status(400).json({ message: "Missing id" });
+    return;
+  }
+
+  if (!name) {
+    res.status(400).json({ message: "Missing name" });
+    return;
+  }
+
+  res.setHeader('Content-Type', contentType(path.extname(name)));
+
+  const blobDocument = await blobsCollection.findOne({ _id: id });
+
+  if (!blobDocument) {
+    res.status(404).json({ message: "Blob not found" });
+    return;
+  }
+
+  const bucket = gcs_storage.bucket(blobDocument.bucket);
+
+  bucket.file(id).createReadStream()
+    .on('error', (error: any) => {
+      console.error(error)
+      res.status(500);
+    })
+    .pipe(res);
+
+  // blobsCollection.updateOne({ _id: req.params.id },
+  //   {
+  //       $set: { },
+  //       $currentDate: { LastAccessed: true }
+  //   });
+}
